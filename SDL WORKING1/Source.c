cbe6ad282e563dcda07eeeae9048e9047a33bba4
arthur fournier <arthur.g.fournier@gmail.com>
2019-01-15 00:59:32 +0100

    changer le code de source.c en quelque chose de plus simple

--- SDL WORKING1/Source.c
+++ SDL WORKING1/Source.c
@@ -1,23 +1,7 @@
 #include "graphics.h"
-#define TailleX 300
-#define TailleY 300
-#define CoteCarre 100
 int main(int argc, char *argv[])
 {
-	POINT CarreBasGauche;
-	POINT CarreHautDroite;
-	POINT CercleCentre;
-	POINT CercleCentre2;
-
-	init_graphics(TailleX, TailleY);
-
-
-	CercleCentre.x = 0.5 * TailleX; CercleCentre.y = 0.5 * TailleY;
-	draw_fill_circle(CercleCentre, 0.45 * CoteCarre, rouge);
-
-
-	CercleCentre2.x = 0.5 * TailleX; CercleCentre2.y = 0.5 * TailleY;
-	draw_fill_circle(CercleCentre2, 0.25 * CoteCarre, noir);
+	init_graphics(800, 600);
 
 
 
