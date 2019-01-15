d3c5d4f126656483f83a7e3ace8e781afd24d0e5
arthur fournier <arthur.g.fournier@gmail.com>
2019-01-15 19:53:43 +0100

    ajout de joueurs

--- SDL WORKING1/Joueurs.c
+++ SDL WORKING1/Joueurs.c
@@ -1,7 +1,14 @@
 #include "graphics.h"
 
-void Joueurs(POINT JoueursCentre);
+void Joueurs(POINT JoueursCentre)
 {
-	POINT JoueursHautBas;
-	POINT JoueursDroiteHaut;
+	POINT BasGauche;
+	POINT HautDroit;
+
+	BasGauche.x = JoueursCentre.x - 10;
+	BasGauche.y = JoueursCentre.y - 100;
+	HautDroit.x = JoueursCentre.x + 10;
+	HautDroit.y = JoueursCentre.y + 100;
+
+	draw_fill_rectangle(BasGauche, HautDroit, blanc);
 }
--- SDL WORKING1/Source.c
+++ SDL WORKING1/Source.c
@@ -1,14 +1,18 @@
 #include "graphics.h"
 void balle(POINT PositionFigure);
+void Joueurs(POINT JoueursCentre);
 
 int main(int argc, char *argv[])
 {
 	POINT PositionFigure;
+	POINT JoueursCentre;
 	init_graphics(800, 600);
 
 	PositionFigure.x = 400; PositionFigure.y = 300;
 	balle(PositionFigure);
 
+	JoueursCentre.x = 20; JoueursCentre.y = 250;
+	Joueurs(JoueursCentre);
 
 	wait_escape();
 	return(0);
