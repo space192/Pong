1f3e5b23ade1afec736cb27ab952951c88bf6178
arthur fournier <arthur.g.fournier@gmail.com>
2019-01-15 17:28:29 +0100

    Balle terminé reste a déterminer comment la bouger

--- SDL WORKING1/Source.c
+++ SDL WORKING1/Source.c
@@ -1,8 +1,13 @@
 #include "graphics.h"
+void balle(POINT PositionFigure);
+
 int main(int argc, char *argv[])
 {
+	POINT PositionFigure;
 	init_graphics(800, 600);
 
+	PositionFigure.x = 400; PositionFigure.y = 300;
+	balle(PositionFigure);
 
 
 	wait_escape();
--- SDL WORKING1/balle.c
+++ SDL WORKING1/balle.c
@@ -1,7 +1,15 @@
 #include "graphics.h"
 
-void balle()
+void balle(POINT FigureCentre)
 {
-	
+	POINT BasGaucheBalle;
+	POINT HautDroitBalle;
+
+	BasGaucheBalle.x = FigureCentre.x - 4;
+	BasGaucheBalle.y = FigureCentre.y - 4;
+	HautDroitBalle.x = FigureCentre.x + 4;
+	HautDroitBalle.y = FigureCentre.y + 4;
+
+	draw_fill_rectangle(BasGaucheBalle, HautDroitBalle, blanc);
 }
 
