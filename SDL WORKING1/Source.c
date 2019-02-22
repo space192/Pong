4755a759e3bfbc18f30b122ccdcbfc8c5fb73235
arthur fournier <arthur.g.fournier@gmail.com>
2019-02-22 11:42:49 +0100

    CA REBONDI TA DARONNE

--- SDL WORKING1/Source.c
+++ SDL WORKING1/Source.c
@@ -16,7 +16,11 @@ int main(int argc, char *argv[])
 	POINT JoueursDeuxCentre;
 	POINT get_arrow();
 	POINT PositionJoueurUn;
+	POINT BasDroitUn;
+	POINT HautDroitUn;
 	POINT PositionJoueurDeux;
+	POINT BasGaucheDeux;
+	POINT HautGaucheDeux;
 	POINT depBalle;
 	POINT anciennePositionBalle;
 	POINT directionBalle;
@@ -27,8 +31,7 @@ int main(int argc, char *argv[])
 	PositionBalle.x = 400; PositionBalle.y = 300;
 	depBalle.x = 1; depBalle.y = 1;
 	
-	ScoresJoueurUn(ScoreJoueurUn);
-	ScoresJoueurDeux(ScoreJoueurDeux);
+
 	PositionJoueurUn.x = 0; PositionJoueurUn.y = 0;
 	PositionJoueurDeux.x = 0; PositionJoueurDeux.y = 0;
 	
@@ -44,45 +47,78 @@ int main(int argc, char *argv[])
 		JoueursDeuxCentre.x = 780; JoueursDeuxCentre.y = 300 + (PositionJoueurDeux.y * 2);
 		Joueurs(JoueursDeuxCentre);
 
-		
-		switch (directionBalle.y)
+		BasDroitUn.x = JoueursUnCentre.x + 10;
+		BasDroitUn.y = JoueursUnCentre.y - 100;
+		HautDroitUn.x = JoueursUnCentre.x + 10;
+		HautDroitUn.y = JoueursUnCentre.y + 100;
+
+		BasGaucheDeux.x = JoueursDeuxCentre.x - 10;
+		BasGaucheDeux.y = JoueursDeuxCentre.y - 100;
+		HautGaucheDeux.x = JoueursDeuxCentre.x - 10;
+		HautGaucheDeux.y = JoueursDeuxCentre.y + 100;
+
+		ScoresJoueurUn(ScoreJoueurUn);
+		ScoresJoueurDeux(ScoreJoueurDeux);
+
+		if (PositionBalle.x == 800)
 		{
-		case 1:
-			PositionBalle.y += depBalle.y;
-			if (PositionBalle.y == 600)
-			{
-				directionBalle.y = 0;
-			}
-			break;
-		case 0:
-			PositionBalle.y -= depBalle.y;
-			if (PositionBalle.y == 0)
-			{
-				directionBalle.y = 1;
-			}
-			break;
+			ScoreJoueurUn++;
+			PositionBalle.x = 400;
+			PositionBalle.y = 300;
 		}
-		switch (directionBalle.x)
+		else if (PositionBalle.x == 0)
 		{
-		case 1:
-			PositionBalle.x += depBalle.x;
-			if (PositionBalle.x == 800)
-			{
-				directionBalle.x = 0;
-			}
-			break;
-		case 0:
-			PositionBalle.x -= depBalle.x;
-			if (PositionBalle.x == 0)
-			{
-				directionBalle.x = 1;
-			}
-			break;
+			ScoreJoueurDeux++;
+			PositionBalle.x = 400;
+			PositionBalle.y = 300;
 		}
-		balle(PositionBalle);
+		else
+		{
+
+			switch (directionBalle.y)
+			{
+			case 1:
+				PositionBalle.y += depBalle.y;
+				if (PositionBalle.y == 600)
+				{
+					directionBalle.y = 0;
+				}
+				break;
+			case 0:
+				PositionBalle.y -= depBalle.y;
+				if (PositionBalle.y == 0)
+				{
+					directionBalle.y = 1;
+				}
+				break;
+			}
+			switch (directionBalle.x)
+			{
+			case 1:
+				PositionBalle.x += depBalle.x;
+				if (PositionBalle.x == 770)
+				{
+					if (PositionBalle.y < HautGaucheDeux.y && PositionBalle.y > BasGaucheDeux.y)
+					{
+						directionBalle.x = 0;
+					}
+				}
+				break;
+			case 0:
+				PositionBalle.x -= depBalle.x;
+				if (PositionBalle.x == 30)
+				{
+					if (PositionBalle.y < HautDroitUn.y && PositionBalle.y > BasDroitUn.y)
+					{
+						directionBalle.x = 1;
+					}
+				}
+				break;
+			}
+			balle(PositionBalle);
+		}
+
 	}
-
-
 	wait_escape();
 	return(0);
 }
